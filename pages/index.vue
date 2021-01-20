cae8bccb92480311fbf4a02ddc01ae8db6d59848
RikuCholo <starcloud.nal.aircraft@gmail.com>
2021-01-20 22:42:25 +0900

    WIP:TODOのHTML作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,83 +1,40 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation </a
-            >.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <section clas="container">
+    <h1>Todoリスト</h1>
+    <div class="addArea">
+      <input
+        type="text"
+        name="addName"
+        id="addName"
+        placeholder="タスクを入力してください"
+      />
+      <button id="addButton" class="button">追加</button>
+    </div>
+    <div class="filter">
+      <button class="button">すべて</button>
+      <button class="button">作業前</button>
+      <button class="button">作業中</button>
+      <button class="button">完了</button>
+    </div>
+    <table class="Lists">
+      <thead>
+        <tr>
+          <th>タスク</th>
+          <th>登録日時</th>
+          <th>状態</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>テスト</td>
+          <td>2020-04-30 17:00</td>
+          <td><button class="button button--yet">作業前</button></td>
+          <td><button class="button button--delete">削除</button></td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
 </template>
 
 <script>
